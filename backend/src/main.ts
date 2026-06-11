8803dd786ca6033c77cc9d298c4c08b026557279
Renat <rinatovich124@gmail.com>
2026-06-11 13:37:12 +0600

    added api for backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.enableCors({ origin: 'http://localhost:5173' });
+  app.enableCors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173'});
 
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Server running on http://localhost:3000`);
