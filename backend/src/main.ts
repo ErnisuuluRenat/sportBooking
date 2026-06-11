032d77ec426021bc4624a60c1ca1bb77118f10aa
Renat <rinatovich124@gmail.com>
2026-06-11 13:41:24 +0600

    updated cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,15 @@ async function bootstrap() {
   
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.enableCors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173'});
+  app.enableCors({
+  origin: [
+    'http://localhost:5173',
+    'https://sport-booking-iota.vercel.app',
+    'https://sport-booking-dx8jbjqoy-renats-projects-25da3b8d.vercel.app',
+    process.env.FRONTEND_URL || 'http://localhost:5173',
+  ],
+  credentials: true,
+})
 
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Server running on http://localhost:3000`);
